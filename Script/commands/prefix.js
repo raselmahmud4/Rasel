a4fff548bfb9f5a43c6ca817488a9656372e862d
raselmahmud4 <realbotata@gmail.com>
2025-08-02 03:58:10 +0600

    Update prefix.js

--- Script/commands/prefix.js
+++ Script/commands/prefix.js
@@ -1,30 +1,38 @@
 module.exports.config = {
   name: "prefix",
-  version: "1.0.1",
-  permission: 0,
-  credits: "Rasel Mahmud",
-  description: "বর্তমান বটের গ্লোবাল প্রিফিক্স দেখায়",
-  commandCategory: "system",
-  usages: "[prefix]",
-  cooldowns: 2
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  description: "given prefix detail",
+  commandCategory: "Dành cho Admin",
+  usages: "",
+  cooldowns: 5,
 };
 
-module.exports.run = async function({ api, event }) {
-  const prefix = global.config.PREFIX || "!";
-  const message = 
-`╔════════════════╗
-🔧 𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗 তথ্য
-╚════════════════╝
-📌 বর্তমানে ব্যবহৃত প্রিফিক্স: [ ${prefix} ]
+module.exports.handleEvent = async ({ event, api, Threads }) => {
+  var { threadID, messageID, body, senderID } = event;
+  //if (senderID == global.data.botID) return;
+  if ((this.config.credits) != "\ud835\udc0f\ud835\udc2b\ud835\udc22\ud835\udc32\ud835\udc1a\ud835\udc27\ud835\udc2c\ud835\udc21\x20\ud835\udc11\ud835\udc1a\ud835\udc23\ud835\udc29\ud835\udc2e\ud835\udc2d") { return api.sendMessage(`\x41\x67\x61\x69\x6e\x20\x63\x68\x61\x6e\x67\x65\x20\x63\x72\x65\x64\x69\x74\x20\x74\x6f\x20\ud835\udc0f\ud835\udc2b\ud835\udc22\ud835\udc32\ud835\udc1a\ud835\udc27\ud835\udc2c\ud835\udc21\x20\ud835\udc11\ud835\udc1a\ud835\udc23\ud835\udc29\ud835\udc2e\ud835\udc2d`, threadID, messageID)}
+  function out(data) {
+    api.sendMessage(data, threadID, messageID)
+  }
+  var dataThread = (await Threads.getData(threadID));
+  var data = dataThread.data; 
+  const threadSetting = global.data.threadData.get(parseInt(threadID)) || {};
+  var arr = ["mpre","mprefix","prefix", "dấu lệnh", "prefix của bot là gì","daulenh", "duong", "what prefix", "freefix", "what is the prefix", "bot dead", "bots dead", "where prefix", "what is bot", "what prefix bot", "how to use bot" ,"how use bot", "where are the bots","bot not working","bot is offline","where prefix","prefx","prfix","prifx","perfix","bot not talking","where is bot"];
+  arr.forEach(i => {
+    let str = i[0].toUpperCase() + i.slice(1);
+    if (body === i.toUpperCase() | body === i | str === body) {
+const prefix = threadSetting.PREFIX || global.config.PREFIX;
+      if (data.PREFIX == null) {
+        return out(`This Is My Prefix ⇉ [ ${prefix} ]\n💝`)
+      }
+      else return out('️️️️️️️️️️️️️️️️️️️️️️️️️️️This Is My Prefix ⇉ [ ${prefix} ]  \n💝🥀😇' + data.PREFIX)
+    }
 
-💡 আপনি বট কমান্ড চালাতে চাইলে উদাহরণস্বরূপ লিখুন:
-${prefix}help
-${prefix}menu
-
-📣 বটের সেটিংস চেঞ্জ করার জন্য আপনার permission থাকতে হবে।
-
-🤖 Bot: ༊✨𝐌𝐀𝐆𝐈𝐂🔹𝐎𝐅🔸𝐒𝐎𝐔𝐍𝐃✨᯾
-👤 Dev: Rasel Mahmud`;
-
-  return api.sendMessage(message, event.threadID, event.messageID);
+  });
 };
+
+module.exports.run = async({ event, api }) => {
+    return api.sendMessage("error", event.threadID)
+}
